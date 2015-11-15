64b7e0be85205bd3e0126c880f5fdeac883e2218
Ryan <ryanhuebert@gmail.com>
2015-11-15 15:36:37 -0800

    Initial Commit

---
+++ lecture28-exercise-multiples-appTests/lecture28_exercise_multiples_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  lecture28_exercise_multiples_appTests.swift
+//  lecture28-exercise-multiples-appTests
+//
+//  Created by Ryan Huebert on 11/15/15.
+//  Copyright © 2015 Ryan Huebert. All rights reserved.
+//
+
+import XCTest
+@testable import lecture28_exercise_multiples_app
+
+class lecture28_exercise_multiples_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

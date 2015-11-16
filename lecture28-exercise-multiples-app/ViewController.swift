7349045e7fd5fe8953b638c015b025da8fcd013d
Ryan <ryanhuebert@gmail.com>
2015-11-15 17:02:14 -0800

    Complete. Keyboard adaptation not implemented.

--- lecture28-exercise-multiples-app/ViewController.swift
+++ lecture28-exercise-multiples-app/ViewController.swift
@@ -9,17 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    // MARK: Properties
+    private let maxMultiple = 12
+    
+    private var total: Int { return lastTotal + base }
+    private var lastTotal: Int { return base * multiple }
+    private var base = 0
+    private var multiple = 0 {
+        didSet {
+            displayLabel?.text = "\(lastTotal) + \(base) = \(total)"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet weak private var titleImage: UIImageView!
+    @IBOutlet weak private var textField: UITextField!
+    @IBOutlet weak private var playButton: UIButton!
+    
+    @IBOutlet weak private var displayLabel: UILabel!
+    @IBOutlet weak private var addButton: UIButton!
+    
+    
+    @IBAction private func playButtonPressed(sender: UIButton) {
+        if let text = textField.text {
+            if let base = Int(text) {
+                
+                titleImage.hidden = true
+                textField.hidden = true
+                playButton.hidden = true
+                
+                displayLabel.hidden = false
+                addButton.hidden = false
+                
+                self.base = base
+                multiple = 0
+                
+            }
+        }
     }
-
-
+    
+    @IBAction private func addButtonPressed(sender: UIButton) {
+        multiple += 1
+        
+        if multiple > maxMultiple {
+            titleImage.hidden = false
+            textField.hidden = false
+            playButton.hidden = false
+            
+            displayLabel.hidden = true
+            addButton.hidden = true
+        }
+    }
 }
 
